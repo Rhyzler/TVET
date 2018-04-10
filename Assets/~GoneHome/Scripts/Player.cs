5710b5966039814192a9185a598c96e5c65a7d6c
Rhyzler <rhysmanson@gmail.com>
2018-04-10 17:59:23 +1000

    began coding

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public string message = "Be Gone THOT!";
+    public Rigidbody rigid;
 
+    float speed = 5;
+
+    /*
+     [] - Brackets
+     {} - Braces
+     () - Parentheses
+
+    Hot Keys:
+         - Clean Code: CRTL + K + D
+         - Fold Code: CTRL + M + O
+         - Un-Fold: CTRL + M + P
+     */
     // Use this for initialization
     void Start()
     {
@@ -14,6 +28,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
     }
 }
