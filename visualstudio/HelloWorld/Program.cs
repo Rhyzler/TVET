58c04285162a7356f91ffbe99eeb0c062af2390c
Rhyzler <rhysmanson@gmail.com>
2018-05-01 17:48:23 +1000

    Created Visual Studio Project

---
+++ visualstudio/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Have you heard the tragedy of Darth Plageius The Wise?";
+
+            // Cooking Recipe / Instructions
+            Console.WriteLine(message);
+            Console.ReadLine(); // Polls for input
+
+            message = "its not a story the jedi would tell you";
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
